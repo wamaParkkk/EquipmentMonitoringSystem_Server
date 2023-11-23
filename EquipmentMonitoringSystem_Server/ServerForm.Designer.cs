9123e44e26adf0b9acb503daf6b5c3cc0e247470
Hyunsoo Park <zahir84@nate.com>
2023-11-23 10:55:41 +0900

    초기 커밋.

---
+++ EquipmentMonitoringSystem_Server/ServerForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace EquipmentMonitoringSystem_Server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServerForm));
+            this.richTextBoxRecvMsg = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxServerStatus = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBoxSendMsg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxRecvMsg
+            // 
+            this.richTextBoxRecvMsg.BackColor = System.Drawing.Color.White;
+            this.richTextBoxRecvMsg.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxRecvMsg.Location = new System.Drawing.Point(15, 309);
+            this.richTextBoxRecvMsg.Name = "richTextBoxRecvMsg";
+            this.richTextBoxRecvMsg.ReadOnly = true;
+            this.richTextBoxRecvMsg.Size = new System.Drawing.Size(800, 582);
+            this.richTextBoxRecvMsg.TabIndex = 6;
+            this.richTextBoxRecvMsg.Text = "";
+            // 
+            // richTextBoxServerStatus
+            // 
+            this.richTextBoxServerStatus.BackColor = System.Drawing.Color.White;
+            this.richTextBoxServerStatus.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxServerStatus.Location = new System.Drawing.Point(15, 29);
+            this.richTextBoxServerStatus.Name = "richTextBoxServerStatus";
+            this.richTextBoxServerStatus.ReadOnly = true;
+            this.richTextBoxServerStatus.Size = new System.Drawing.Size(800, 240);
+            this.richTextBoxServerStatus.TabIndex = 7;
+            this.richTextBoxServerStatus.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Server status";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(12, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Message received from the client";
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "ClientConnect.png");
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(740, 897);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 25);
+            this.btnSend.TabIndex = 13;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // textBoxSendMsg
+            // 
+            this.textBoxSendMsg.BackColor = System.Drawing.Color.White;
+            this.textBoxSendMsg.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSendMsg.ForeColor = System.Drawing.Color.Black;
+            this.textBoxSendMsg.Location = new System.Drawing.Point(15, 898);
+            this.textBoxSendMsg.Name = "textBoxSendMsg";
+            this.textBoxSendMsg.Size = new System.Drawing.Size(719, 25);
+            this.textBoxSendMsg.TabIndex = 12;
+            this.textBoxSendMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSendMsg_KeyDown);
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1214, 935);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.textBoxSendMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBoxServerStatus);
+            this.Controls.Add(this.richTextBoxRecvMsg);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ServerForm";
+            this.Text = "Equipment Monitoring System Server";
+            this.Activated += new System.EventHandler(this.ServerForm_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ServerForm_FormClosing);
+            this.Load += new System.EventHandler(this.ServerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxRecvMsg;
+        private System.Windows.Forms.RichTextBox richTextBoxServerStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBoxSendMsg;
+    }
+}
+
---
+++ EquipmentMonitoringSystem_Server/ServerForm.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EquipmentMonitoringSystem_Server
+{
+    public partial class ServerForm : Form
+    {
+        private Socket m_ServerSocket;
+        private List<Socket> m_ClientSocket;        
+        private byte[] szData;
+
+        public ServerForm()
+        {
+            InitializeComponent();
+
+            _Init_Server();
+        }
+
+        private void ServerForm_Load(object sender, EventArgs e)
+        {
+            Top = 10;
+            Left = 10;            
+        }
+
+        private void ServerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {           
+            Dispose();
+            //Application.Exit();
+            Application.ExitThread();
+            Environment.Exit(0);
+        }
+
+        private void ServerForm_Activated(object sender, EventArgs e)
+        {
+            SetDoubleBuffered(richTextBoxServerStatus);
+            SetDoubleBuffered(richTextBoxRecvMsg);
+        }
+
+        private void SetDoubleBuffered(Control control, bool doubleBuffered = true)
+        {
+            PropertyInfo propertyInfo = typeof(Control).GetProperty
+            (
+                "DoubleBuffered",
+                BindingFlags.Instance | BindingFlags.NonPublic
+            );
+            propertyInfo.SetValue(control, doubleBuffered, null);
+        }
+
+        private void _Init_Server()
+        {
+            m_ClientSocket = new List<Socket>();            
+            m_ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            
+            // Server IP 및 Port
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 8000);
+
+            // Server binding
+            m_ServerSocket.Bind(iPEndPoint);
+            m_ServerSocket.Listen(10);
+
+            // Socket event
+            SocketAsyncEventArgs socketAsyncEventArgs = new SocketAsyncEventArgs();
+            // Event 발생 시, Accept_Completed 함수 실행
+            socketAsyncEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(_Accept_Completed);
+
+            // Waiting for client connection
+            m_ServerSocket.AcceptAsync(socketAsyncEventArgs);            
+        }
+
+        /*
+         * Client connection acceptance callback function
+         */
+        private void _Accept_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket clientSocket = e.AcceptSocket;            
+
+            // 요청 Socket을 수락 후 리스트에 추가
+            m_ClientSocket.Add(clientSocket);
+            
+            if(m_ClientSocket != null)
+            {
+                DisplayText_ServerStatus("<< " + clientSocket.RemoteEndPoint.ToString() + " >>" + " is connected");
+
+                SocketAsyncEventArgs socketAsyncEventArgs = new SocketAsyncEventArgs();                
+
+                // 수신용 buffer 할당
+                szData = new byte[1024];                
+                socketAsyncEventArgs.SetBuffer(szData, 0, 1024);
+                socketAsyncEventArgs.UserToken = m_ClientSocket;
+                socketAsyncEventArgs.Completed += new EventHandler<SocketAsyncEventArgs>(_Receive_Completed);
+
+                // 수락 된 Socket의 데이터 수신 대기
+                clientSocket.ReceiveAsync(socketAsyncEventArgs);
+            }
+
+            e.AcceptSocket = null;
+            // 요청 Socket 처리 후 다시 수락 대기
+            m_ServerSocket.AcceptAsync(e);
+        }
+
+        /*
+         * Data receive callback function
+         */
+        private void _Receive_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket clientSocket = (Socket)sender;
+
+            try
+            {
+                // 해당 Socket의 접속 유무 확인 후 false면 Socket을 닫음
+                if (clientSocket.Connected && e.BytesTransferred > 0)
+                {
+                    // Data receive                
+                    byte[] szData = e.Buffer;
+                    string strData = Encoding.Unicode.GetString(szData);
+                    string recvMsg = strData.Replace("\0", "").Trim();
+                    DisplayText("<< " + clientSocket.RemoteEndPoint.ToString() + " >>" + recvMsg);
+
+                    // 장비 Client로부터 받은 데이터를 Monitoring client로 전송
+                    for (int i = m_ClientSocket.Count - 1; i >= 0; i--)
+                    {
+                        Socket socket = m_ClientSocket[i];
+                        try
+                        {
+                            socket.Send(e.Buffer);
+                        }
+                        catch
+                        {
+                            // 오류가 발생하면 전송 취소하고 리스트에서 삭제한다.
+                            try
+                            {
+                                socket.Dispose();
+                            }
+                            catch (SocketException)
+                            {
+
+                            }
+
+                            m_ClientSocket.RemoveAt(i);
+                        }
+                    }
+
+                    for (int i = 0; i < szData.Length; i++)
+                    {
+                        szData[i] = 0;
+                    }
+
+                    e.SetBuffer(szData, 0, 1024);
+                    clientSocket.ReceiveAsync(e);
+                }
+                else
+                {
+                    // Socket 재사용 유무
+                    clientSocket.Disconnect(false);
+
+                    // Client socket 리스트에서 해당 Socket 삭제
+                    m_ClientSocket.Remove(clientSocket);
+                    DisplayText_ServerStatus("<< " + clientSocket.RemoteEndPoint.ToString() + " >>" + " is disconnected");
+                }
+            }
+            catch (SocketException se)
+            {
+                MessageBox.Show(se.Message, "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }            
+        }
+
+        public void _Begin_Send(string message)
+        {
+            byte[] buffer = Encoding.Unicode.GetBytes(message);
+
+            for (int i = m_ClientSocket.Count - 1; i >= 0; i--)
+            {
+                Socket socket = m_ClientSocket[i];
+                try
+                {
+                    socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+                    DisplayText(message);
+                }
+                catch (SocketException)
+                {
+                    
+                }
+            }
+        }
+
+        private void textBoxSendMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                _Begin_Send(textBoxSendMsg.Text);
+
+                textBoxSendMsg.Clear();
+                textBoxSendMsg.Focus();
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            _Begin_Send(textBoxSendMsg.Text);
+
+            textBoxSendMsg.Clear();
+            textBoxSendMsg.Focus();
+        }
+
+        private void DisplayText_ServerStatus(string text)
+        {            
+            if (richTextBoxServerStatus.InvokeRequired)
+            {                
+                richTextBoxServerStatus.BeginInvoke(new MethodInvoker(delegate
+                {
+                    if (richTextBoxServerStatus.Lines.Length > 100)
+                    {
+                        richTextBoxServerStatus.Clear();
+                    }
+
+                    richTextBoxServerStatus.AppendText("[ " + DateTime.Now.ToString() + "] " + text + Environment.NewLine);
+                    richTextBoxServerStatus.ScrollToCaret();
+                }));
+            }
+            else
+            {
+                richTextBoxServerStatus.AppendText("[ " + DateTime.Now.ToString() + "] " + text + Environment.NewLine);
+                richTextBoxServerStatus.ScrollToCaret();
+            }
+        }
+
+        private void DisplayText(string text) 
+        {            
+            if (richTextBoxRecvMsg.InvokeRequired) 
+            {                
+                richTextBoxRecvMsg.BeginInvoke(new MethodInvoker(delegate 
+                {
+                    if (richTextBoxRecvMsg.Lines.Length > 100)
+                    {
+                        richTextBoxRecvMsg.Clear();
+                    }
+
+                    richTextBoxRecvMsg.AppendText("[ " + DateTime.Now.ToString() + "] " + text + Environment.NewLine);
+                    richTextBoxRecvMsg.ScrollToCaret();
+                })); 
+            } 
+            else
+            {
+                richTextBoxRecvMsg.AppendText("[ " + DateTime.Now.ToString() + "] " + text + Environment.NewLine);
+                richTextBoxRecvMsg.ScrollToCaret();
+            }                
+        }        
+    }
+}
